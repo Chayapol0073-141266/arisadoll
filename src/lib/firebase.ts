3b88f5beb126f94afb60d17e6b462f210e5f6598
Kamo0073 <chayapol.toon@gmail.com>
2026-03-17 16:25:10 +0700

    fix: Update Firebase project ID
    The Firebase project ID was incorrectly configured, leading to potential issues with Firebase services integration. This commit corrects the `projectId` in the `firebaseConfig` object to `arisadb-b8936`.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyAwuo4Mn2Pq3KO-HJ6tJCy21Ik45k7MADQ",
   authDomain: "arisadb-b8936.firebaseapp.com",
-  projectId: "arisadb-b8936"
+  projectId: "arisadb-b8936",
   storageBucket: "arisadb-b8936.firebasestorage.app",
   messagingSenderId: "227600459327",
   appId: "1:227600459327:web:6b5470d6bcd670c72e3a8a",
